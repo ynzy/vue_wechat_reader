4c22a5126fc71e0d7fe99d2e51183b402b8e8733
zhangyong <758922096@qq.com>
2020-08-25 18:09:35 +0800

    feat(model:all): 添加路由

--- wechat_reader_admin/src/router/index.js
+++ wechat_reader_admin/src/router/index.js
@@ -6,34 +6,6 @@ Vue.use(Router)
 /* Layout */
 import Layout from '@/layout'
 
-
-
-/**
- * Note: sub-menu only appear when route children.length >= 1
- * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
- *
- * hidden: true                   if set true, item will not show in the sidebar(default is false)
- * alwaysShow: true               if set true, will always show the root menu
- *                                if not set alwaysShow, when item has more than one children route,
- *                                it will becomes nested mode, otherwise not show the root menu
- * redirect: noRedirect           if set noRedirect will no redirect in the breadcrumb
- * name:'router-name'             the name is used by <keep-alive> (must set!!!)
- * meta : {
-    roles: ['admin','editor']    control the page roles (you can set multiple roles)
-    title: 'title'               the name show in sidebar and breadcrumb (recommend set)
-    icon: 'svg-name'             the icon show in the sidebar
-    noCache: true                if set true, the page will no be cached(default is false)
-    affix: true                  if set true, the tag will affix in the tags-view
-    breadcrumb: false            if set false, the item will hidden in breadcrumb(default is true)
-    activeMenu: '/example/list'  if set path, the sidebar will highlight the path you set
-  }
- */
-
-/**
- * constantRoutes
- * a base page that does not have permission requirements
- * all roles can be accessed
- */
 export const constantRoutes = [
   {
     path: '/redirect',
@@ -81,36 +53,21 @@ export const constantRoutes = [
   },
 ]
 
-/**
- * asyncRoutes
- * the routes that need to be dynamically loaded based on user roles
- */
+
 export const asyncRoutes = [
-  /* {
-    path: '/error',
+  {
+    path: '/book',
     component: Layout,
-    redirect: 'noRedirect',
-    name: 'ErrorPages',
-    meta: {
-      title: 'Error Pages',
-      icon: '404'
-    },
+    redirect: '/book/create',
+    meta: { title: '图书管理', icon: 'documentation', roles: ['admin'] },
     children: [
       {
-        path: '401',
-        component: () => import('@/views/error-page/401'),
-        name: 'Page401',
-        meta: { title: '401', noCache: true }
-      },
-      {
-        path: '404',
-        component: () => import('@/views/error-page/404'),
-        name: 'Page404',
-        meta: { title: '404', noCache: true }
+        path: '/book/create',
+        component: () => import('@/views/book/create'),
+        meta: { title: '上传图书', icon: 'edit', roles: ['admin'] },
       }
     ]
-  }, */
-  // 404 page must be placed at the end !!!
+  },
   { path: '*', redirect: '/404', hidden: true }
 ]
 
---
+++ wechat_reader_admin/src/views/book/create.vue
@@ -0,0 +1,16 @@
+<!--  -->
+<template>
+  <div>111</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {}
+  },
+  methods: {},
+  mounted() {}
+}
+</script>
+<style lang="scss" scoped></style>
