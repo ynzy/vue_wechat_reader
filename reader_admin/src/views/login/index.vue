a5a9fda114f3bdcc285096274b4888f7a009fa8f
zhangyong <758922096@qq.com>
2020-09-02 16:59:14 +0800

    feat(model:all): 模块化打包

--- reader_admin/src/views/login/index.vue
+++ reader_admin/src/views/login/index.vue
@@ -9,7 +9,7 @@
       label-position="left"
     >
       <div class="title-container">
-        <h3 class="title">读书管理后天</h3>
+        <h3 class="title">读书管理后台</h3>
       </div>
       <el-form-item prop="username">
         <span class="svg-container">
